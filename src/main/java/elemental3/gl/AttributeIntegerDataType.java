6b3bdd7a80efd15f2133432ec438c67c4aa2b148
Peter Donald <peter@realityforge.org>
2021-01-31 20:39:20 +1100

    Add data type enum

---
+++ src/main/java/elemental3/gl/AttributeIntegerDataType.java
@@ -0,0 +1,37 @@
+package elemental3.gl;
+
+import java.lang.annotation.Documented;
+import javax.annotation.Generated;
+import org.intellij.lang.annotations.MagicConstant;
+
+@Generated("org.realityforge.webtack")
+@Documented
+@MagicConstant(
+    intValues = {
+        WebGL2RenderingContext.BYTE,
+        WebGL2RenderingContext.UNSIGNED_BYTE,
+        WebGL2RenderingContext.SHORT,
+        WebGL2RenderingContext.UNSIGNED_SHORT
+    }
+)
+public @interface AttributeIntegerDataType {
+  final class Validator {
+    private Validator() {
+    }
+
+    @AttributeIntegerDataType
+    @SuppressWarnings("MagicConstant")
+    public static int cast(final int value) {
+      assertValid( value );
+      return value;
+    }
+
+    public static void assertValid(final int value) {
+      assert isValid( value ) : "@AttributeIntegerDataType annotated value must be one of [WebGL2RenderingContext.BYTE, WebGL2RenderingContext.UNSIGNED_BYTE, WebGL2RenderingContext.SHORT, WebGL2RenderingContext.UNSIGNED_SHORT] but is " + value;
+    }
+
+    public static boolean isValid(final int value) {
+      return WebGL2RenderingContext.BYTE == value || WebGL2RenderingContext.UNSIGNED_BYTE == value || WebGL2RenderingContext.SHORT == value || WebGL2RenderingContext.UNSIGNED_SHORT == value;
+    }
+  }
+}
