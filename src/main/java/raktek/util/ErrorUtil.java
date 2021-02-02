5365a30a53e1ed4d6bd226e798ff58c5f39c72f6
Peter Donald <peter@realityforge.org>
2021-02-02 20:16:52 +1100

    Introduce an error util to help with tracking errors

---
+++ src/main/java/raktek/util/ErrorUtil.java
@@ -0,0 +1,65 @@
+package raktek.util;
+
+import elemental3.gl.WebGL2RenderingContext;
+import elemental3.gl.WebGLContextError;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public final class ErrorUtil
+{
+  private ErrorUtil()
+  {
+  }
+
+  @Nullable
+  public static String describeContextErrors( @Nonnull final WebGL2RenderingContext gl )
+  {
+    int code;
+    String message = null;
+    while ( WebGL2RenderingContext.NO_ERROR != ( code = gl.getError() ) )
+    {
+      final String contextError = describeContextError( code );
+      message = null == message ? contextError : message + "\n" + contextError;
+    }
+    return message;
+  }
+
+  @Nonnull
+  public static String describeContextError( @WebGLContextError final int value )
+  {
+    WebGLContextError.Validator.assertValid( value );
+
+    if ( WebGL2RenderingContext.NO_ERROR == value )
+    {
+      return "No error has been recorded.";
+    }
+    else if ( WebGL2RenderingContext.INVALID_ENUM == value )
+    {
+      return "An unacceptable value has been specified for an enumerated argument.";
+    }
+    else if ( WebGL2RenderingContext.INVALID_VALUE == value )
+    {
+      return "A numeric argument is out of range.";
+    }
+    else if ( WebGL2RenderingContext.INVALID_OPERATION == value )
+    {
+      return "The specified command is not allowed for the current state.";
+    }
+    else if ( WebGL2RenderingContext.INVALID_FRAMEBUFFER_OPERATION == value )
+    {
+      return "The currently bound framebuffer is not framebuffer complete when trying to render to or to read from it.";
+    }
+    else if ( WebGL2RenderingContext.OUT_OF_MEMORY == value )
+    {
+      return "Not enough memory is left to execute the command.";
+    }
+    else if ( WebGL2RenderingContext.CONTEXT_LOST_WEBGL == value )
+    {
+      return "The WebGL context has been lost.";
+    }
+    else
+    {
+      return "Unknown WebGL error code=" + value;
+    }
+  }
+}
---
+++ src/test/java/raktek/util/ErrorUtilTest.java
@@ -0,0 +1,52 @@
+package raktek.util;
+
+import elemental3.gl.WebGL2RenderingContext;
+import org.testng.annotations.Test;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+
+public final class ErrorUtilTest
+{
+  @Test
+  public void getContextError()
+  {
+    final WebGL2RenderingContext gl = mock( WebGL2RenderingContext.class );
+
+    when( gl.getError() ).thenReturn( WebGL2RenderingContext.NO_ERROR,
+                                      WebGL2RenderingContext.INVALID_VALUE,
+                                      WebGL2RenderingContext.OUT_OF_MEMORY,
+                                      WebGL2RenderingContext.CONTEXT_LOST_WEBGL,
+                                      WebGL2RenderingContext.NO_ERROR );
+
+    assertNull( ErrorUtil.describeContextErrors( gl ) );
+    assertEquals( ErrorUtil.describeContextErrors( gl ),
+                  "A numeric argument is out of range.\n" +
+                  "Not enough memory is left to execute the command.\n" +
+                  "The WebGL context has been lost." );
+  }
+
+  @Test
+  public void describeContextError()
+  {
+    assertEquals( ErrorUtil.describeContextError( WebGL2RenderingContext.NO_ERROR ),
+                  "No error has been recorded." );
+    assertEquals( ErrorUtil.describeContextError( WebGL2RenderingContext.INVALID_ENUM ),
+                  "An unacceptable value has been specified for an enumerated argument." );
+    assertEquals( ErrorUtil.describeContextError( WebGL2RenderingContext.INVALID_OPERATION ),
+                  "The specified command is not allowed for the current state." );
+    assertEquals( ErrorUtil.describeContextError( WebGL2RenderingContext.INVALID_VALUE ),
+                  "A numeric argument is out of range." );
+    assertEquals( ErrorUtil.describeContextError( WebGL2RenderingContext.OUT_OF_MEMORY ),
+                  "Not enough memory is left to execute the command." );
+    assertEquals( ErrorUtil.describeContextError( WebGL2RenderingContext.INVALID_FRAMEBUFFER_OPERATION ),
+                  "The currently bound framebuffer is not framebuffer complete when trying to render to or to read from it." );
+    assertEquals( ErrorUtil.describeContextError( WebGL2RenderingContext.CONTEXT_LOST_WEBGL ),
+                  "The WebGL context has been lost." );
+
+    @SuppressWarnings( { "MagicConstant", "ResultOfMethodCallIgnored" } )
+    final AssertionError assertionError =
+      expectThrows( AssertionError.class, () -> ErrorUtil.describeContextError( -1 ) );
+    assertEquals( assertionError.getMessage(),
+                  "@WebGLContextError annotated value must be one of [WebGL2RenderingContext.NO_ERROR, WebGL2RenderingContext.INVALID_ENUM, WebGL2RenderingContext.INVALID_VALUE, WebGL2RenderingContext.INVALID_OPERATION, WebGL2RenderingContext.INVALID_FRAMEBUFFER_OPERATION, WebGL2RenderingContext.OUT_OF_MEMORY, WebGL2RenderingContext.CONTEXT_LOST_WEBGL] but is -1" );
+  }
+}
