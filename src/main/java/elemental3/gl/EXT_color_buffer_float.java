421cb495b11ac130618aa7d347054a374919e6ca
Peter Donald <peter@realityforge.org>
2021-02-15 08:52:49 +1100

    Regenerate

---
+++ src/main/java/elemental3/gl/EXT_color_buffer_float.java
@@ -0,0 +1,22 @@
+package elemental3.gl;
+
+import javax.annotation.Generated;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+/**
+ * The EXT_color_buffer_float extension is part of WebGL and adds the ability to render a variety of floating point formats.
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EXT_color_buffer_float">EXT_color_buffer_float - MDN</a>
+ * @see <a href="https://www.khronos.org/registry/webgl/extensions/EXT_color_buffer_float/">EXT_color_buffer_float - EXT_color_buffer_float</a>
+ */
+@Generated("org.realityforge.webtack")
+@JsType(
+    isNative = true,
+    namespace = JsPackage.GLOBAL,
+    name = "Object"
+)
+public class EXT_color_buffer_float {
+  protected EXT_color_buffer_float() {
+  }
+}
