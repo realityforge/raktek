aa4142093a5bbc2e67a1df348cf20791b3d505c1
Peter Donald <peter@realityforge.org>
2021-02-18 09:21:01 +1100

    Regenerate

--- src/main/java/elemental3/Global.java
+++ src/main/java/elemental3/Global.java
@@ -4020,6 +4020,147 @@ public final class Global {
     globalThis().removeEventListener(type, callback);
   }
 
+  /**
+   * The decodeURI() function decodes a Uniform Resource Identifier (URI) previously created by encodeURI() or by a similar routine.
+   *
+   * @return A new string representing the unencoded version of the given encoded Uniform Resource Identifier (URI).
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/decodeURI">decodeURI - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-decodeuri-encodeduri">decodeURI() - ECMA</a>
+   */
+  @Nonnull
+  public static String decodeURI(@Nonnull String encodedURI) {
+    return globalThis().decodeURI(encodedURI);
+  }
+
+  /**
+   * The decodeURIComponent() function decodes a Uniform Resource Identifier (URI) component previously created by encodeURIComponent or by a similar routine.
+   *
+   * @return A new string representing the decoded version of the given encoded Uniform Resource Identifier (URI) component.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/decodeURIComponent">decodeURIComponent - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-decodeuricomponent-encodeduricomponent">decodeURIComponent() - ECMA</a>
+   */
+  @Nonnull
+  public static String decodeURIComponent(@Nonnull String encodedURI) {
+    return globalThis().decodeURIComponent(encodedURI);
+  }
+
+  /**
+   * The encodeURI() function encodes a URI by replacing each instance of certain characters by one, two, three, or four escape sequences representing the UTF-8 encoding of the character (will only be four escape sequences for characters composed of two "surrogate" characters).
+   *
+   * @return a new string representing the provided string encoded as a URI.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURI">encodeURI - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-encodeuri-uri">encodeURI() - ECMA</a>
+   */
+  @Nonnull
+  public static String encodeURI(@Nonnull String uri) {
+    return globalThis().encodeURI(uri);
+  }
+
+  /**
+   * The encodeURIComponent() function encodes a URI by replacing each instance of certain characters by one, two, three, or four escape sequences representing the UTF-8 encoding of the character (will only be four escape sequences for characters composed of two "surrogate" characters).
+   *
+   * @return a new string representing the provided string encoded as a URI component.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent">encodeURIComponent - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-encodeuricomponent-uricomponent">encodeURIComponent() - ECMA</a>
+   */
+  @Nonnull
+  public static String encodeURIComponent(@Nonnull String uri) {
+    return globalThis().encodeURIComponent(uri);
+  }
+
+  /**
+   * The escape() function computes a new string in which certain characters have been replaced by a hexadecimal escape sequence.
+   *
+   * @return a new string in which certain characters have been escaped.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/escape">escape - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-escape-string">escape() - ECMA</a>
+   * @deprecated
+   */
+  @Nonnull
+  public static String escape(@Nonnull String str) {
+    return globalThis().escape(str);
+  }
+
+  /**
+   * The eval() function evaluates JavaScript code represented as a string.
+   *
+   * @return The completion value of evaluating the given code. If the completion value is empty, undefined is returned.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval">eval - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-eval-x">eval() - ECMA</a>
+   */
+  @Nullable
+  public static Any eval(@Nonnull String code) {
+    return globalThis().eval(code);
+  }
+
+  /**
+   * The global isFinite() function determines whether the passed value is a finite number.
+   *
+   * @return false if the specified value positive or negative infinity or NaN else true.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isFinite">isFinite - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-isfinite-number">isFinite() - ECMA</a>
+   */
+  public static boolean isFinite(double num) {
+    return globalThis().isFinite(num);
+  }
+
+  /**
+   * The isNaN() function determines whether a value is NaN or not. Note, coercion inside the isNaN function has interesting rules; you may alternatively want to use Number.isNaN(), as defined in ECMAScript 2015.
+   *
+   * @return true if the specified value is NaN else false.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN">isNaN - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-isnan-number">isNaN() - ECMA</a>
+   */
+  public static boolean isNaN(double value) {
+    return globalThis().isNaN(value);
+  }
+
+  /**
+   * The parseFloat() function parses an argument (converting it to a string first if needed) and returns a floating point number.
+   *
+   * @return a floating point number parsed from the given string.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat">parseFloat - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-parsefloat-string">parseFloat() - ECMA</a>
+   */
+  public static double parseFloat(@Nonnull String string) {
+    return globalThis().parseFloat(string);
+  }
+
+  /**
+   * The parseInt() function parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems).
+   *
+   * @return an integer parsed from the given string.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt">parseInt - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-parseint-string-radix">parseInt() - ECMA</a>
+   */
+  public static int parseInt(@Nonnull String string, int radix) {
+    return globalThis().parseInt(string, radix);
+  }
+
+  /**
+   * The parseInt() function parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems).
+   *
+   * @return an integer parsed from the given string.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt">parseInt - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-parseint-string-radix">parseInt() - ECMA</a>
+   */
+  public static int parseInt(@Nonnull String string) {
+    return globalThis().parseInt(string);
+  }
+
+  /**
+   * The unescape() function computes a new string in which hexadecimal escape sequences are replaced with the character that it represents. The escape sequences might be introduced by a function like escape. Usually, decodeURI or decodeURIComponent are preferred over unescape.
+   *
+   * @return a new string in which certain characters have been unescaped.
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/unescape">unescape - MDN</a>
+   * @see <a href="https://tc39.es/ecma262/#sec-unescape-string">unescape() - ECMA</a>
+   * @deprecated
+   */
+  @Nonnull
+  public static String unescape(@Nonnull String str) {
+    return globalThis().unescape(str);
+  }
+
   @Nonnull
   public static ConsoleNamespace console() {
     return globalThis().console();
