388939a17d1045c92336935215911881edbe1c1a
Peter Donald <peter@realityforge.org>
2021-02-19 11:37:32 +1100

    Add method for creating descriptor

--- src/main/java/raktek/util/ProgramDescriptor.java
+++ src/main/java/raktek/util/ProgramDescriptor.java
@@ -1,8 +1,18 @@
 package raktek.util;
 
+import elemental3.Console;
+import elemental3.gl.AttributeDataType;
+import elemental3.gl.UniformDataType;
+import elemental3.gl.WebGL2RenderingContext;
+import elemental3.gl.WebGLActiveInfo;
+import elemental3.gl.WebGLProgram;
+import elemental3.gl.WebGLUniformLocation;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import javax.annotation.Nonnull;
+import jsinterop.base.Any;
 import raktek.Raktek;
 
 public final class ProgramDescriptor
@@ -14,9 +24,9 @@ public final class ProgramDescriptor
   @Nonnull
   private final UniformDescriptor[] _uniforms;
 
-  public ProgramDescriptor( @Nonnull final String name,
-                            @Nonnull final AttributeDescriptor[] attributes,
-                            @Nonnull final UniformDescriptor[] uniforms )
+  private ProgramDescriptor( @Nonnull final String name,
+                             @Nonnull final AttributeDescriptor[] attributes,
+                             @Nonnull final UniformDescriptor[] uniforms )
   {
     _name = Objects.requireNonNull( name );
     _attributes = Objects.requireNonNull( attributes );
@@ -66,5 +76,60 @@ public final class ProgramDescriptor
            ",attributes=" + Arrays.asList( _attributes ) +
            ",uniforms=" + Arrays.asList( _uniforms ) +
            "]";
+  }
+
+  @Nonnull
+  static ProgramDescriptor create( @Nonnull final WebGL2RenderingContext gl,
+                                   @Nonnull final String programName,
+                                   @Nonnull final AttributeLocation[] attributeLocations,
+                                   @Nonnull final WebGLProgram program )
+  {
+    final Any activeUniforms = gl.getProgramParameter( program, WebGL2RenderingContext.ACTIVE_UNIFORMS );
+    assert null != activeUniforms;
+    final int uniformCount = activeUniforms.asInt();
+    final List<UniformDescriptor> uniforms = new ArrayList<>();
+    for ( int i = 0; i < uniformCount; i++ )
+    {
+      final WebGLActiveInfo info = gl.getActiveUniform( program, i );
+      assert null != info;
+      final String name = info.name();
+      final WebGLUniformLocation location = gl.getUniformLocation( program, name );
+      assert null != location;
+      final UniformDescriptor uniform =
+        new UniformDescriptor( name, UniformDataType.Validator.cast( info.type() ), location );
+      uniforms.add( uniform );
+      Console.log( uniform.toString() );
+    }
+    final Any activeAttributes = gl.getProgramParameter( program, WebGL2RenderingContext.ACTIVE_ATTRIBUTES );
+    assert null != activeAttributes;
+    final int attributeCount = activeAttributes.asInt();
+    final List<AttributeDescriptor> attributes = new ArrayList<>();
+    for ( int i = 0; i < attributeCount; i++ )
+    {
+      final WebGLActiveInfo info = gl.getActiveAttrib( program, i );
+      assert null != info;
+
+      int index = WebGL2RenderingContext.INVALID_INDEX;
+      final String name = info.name();
+      for ( final AttributeLocation location : attributeLocations )
+      {
+        if ( location.getName().equals( name ) )
+        {
+          index = location.getIndex();
+        }
+      }
+      if ( WebGL2RenderingContext.INVALID_INDEX == index )
+      {
+        index = gl.getAttribLocation( program, name );
+        assert WebGL2RenderingContext.INVALID_INDEX != index;
+      }
+      final AttributeDescriptor attribute =
+        new AttributeDescriptor( name, AttributeDataType.Validator.cast( info.type() ), index );
+      attributes.add( attribute );
+      Console.log( attribute.toString() );
+    }
+    return new ProgramDescriptor( programName,
+                                  attributes.toArray( new AttributeDescriptor[ 0 ] ),
+                                  uniforms.toArray( new UniformDescriptor[ 0 ] ) );
   }
 }
