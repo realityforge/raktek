1b96d2cb2e5f0cbf26343c364eeff471d1a6117d
Peter Donald <peter@realityforge.org>
2021-02-21 12:58:36 +1100

    Add util to calculate the number of primitives given specified attribute data

--- src/main/java/raktek/util/Attribute.java
+++ src/main/java/raktek/util/Attribute.java
@@ -19,6 +19,24 @@ public final class Attribute
   @Nonnull
   private final Accessor _accessor;
 
+  /**
+   * Return the number of vertices using the the specified attributes,
+   * assuming that every attribute makes use of the entire buffer.
+   *
+   * @param attributes the attributes.
+   * @return the number of vertices using the the specified attributes.
+   */
+  public static int getVertexCount( @Nonnull final Attribute... attributes )
+  {
+    assert 0 != attributes.length;
+    int count = Integer.MAX_VALUE;
+    for ( final Attribute attribute : attributes )
+    {
+      count = Math.min( count, attribute.getVertexCount() );
+    }
+    return count;
+  }
+
   public Attribute( @Nonnull final String name, @Nonnull final Buffer buffer, @Nonnull final Accessor accessor )
   {
     _name = Objects.requireNonNull( name );
